ed17c5d75877d840a85746389eeb754671d133a5
Alexander George Legaspi <lex83nyc@gmail.com>
2018-06-08 12:37:36 -0400

    updated the freestyle erb

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'mailgun'
 require './models'
 
+
 set :session_secret, ENV['RUMBLR_SESSION_SECRET']
 enable :sessions
 
@@ -25,14 +26,16 @@ enable :sessions
 
     #this post method is called on signup and executes
         post('/signup') do
-            #we create a variable that finds the current users email
+            #we create a variable that finds an existing user with this email
             existing_user = User.find_by(email: params[:email])
-            #if the variable has value then they're redirected to login
+            #if we were to find exisiting user then we want to prevent signing up with same email so we redirect user to login page. 
             if existing_user != nil
                 return redirect '/login'
             end
-            #we create a variable instance of User with all the values submitted.
+            
+            #we create a variable instance of User with all the values submitted, and creates this row in the database.
             user = User.create(
+                #columns           #values
                 first_name: params[:first_name],
                 last_name: params[:last_name],
                 nickname: params[:nickname],
@@ -44,44 +47,55 @@ enable :sessions
             redirect '/dashboard'
         end
 
-        #this method for login and executes
+        #this method is for login and executes
         get('/login') do 
             #if the user is logged in we automatically send them to the dashboard
-            if session[:user_id] == true
+            if session[:user_id]
                 return redirect '/dashboard'
             end
             #if not they will see contents of the login embed ruby
             erb :login	
         end
 
-        #this mthod call the login and executes
+        #this POST method calls the login and executes
         post('/login') do
-            #we create a variable instance of the current users email
+            
+            
+            
+            
+            
+            #we're finding a user by email and saving it in a variable
             user = User.find_by(email: params[:email])
-            #then we check to see if it has a value if not then they're sent to login
+            #if there is not a user that exists with an email then user is redirected
             if user.nil?
                 puts "Invalid UN: #{params[:email]}"
                 return redirect '/login'
             end
-            #    
+            #checking if password types equals to passwor in database    
             unless user.password == params[:password]
+                #if password does not match then it will log the following
                 puts "Invalid PW: #{params[:password]}, expected: #{user.password}"
                 return redirect '/login'
             end
-
+            
+            #we set session user id to log them in and redirect
             session[:user_id] = user.id
             redirect '/dashboard'
         end
 
 
         get('/dashboard') do
-            user_id = session[:user_id]
-            if user_id.nil?
+            #this check to see if the user is logged in if not then redirect 
+            if session[:user_id].nil?
                 return redirect '/'
             end
 
+            #creating instance var of the last 20 items the Entry table.
             @entries = Entry.last(20)
-            @user = User.find(user_id)
+            #creating an instance var that allows to call all the column values (aka methods)
+            
+            @user = User.find(session[:user_id])
+            
             erb :dashboard
         end	
 
